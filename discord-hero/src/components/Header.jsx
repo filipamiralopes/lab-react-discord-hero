97040b3436754bfc818a73732e6e84ee9a482c71
Filipa <filipamnmlopes@hotmail.com>
2024-05-27 13:56:55 +0100

    header done

--- discord-hero/src/components/Header.jsx
+++ discord-hero/src/components/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="header">
+      <h1>
+        IMAGINE A <br />
+        PLACE...
+      </h1>
+      <p>
+        ...where you can belong to a school club, a gaming group, or a worldwide
+        art community. Where just you and a handful of friends can spend time
+        together. A place that makes it easy to talk every day and hang out more
+        often.
+      </p>
+      <button id="download-btn">Download for Mac</button>
+      <button id="open-btn">Open Discord in your browser</button>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
